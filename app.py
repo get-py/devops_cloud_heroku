0e9fd7d0e086c8ecc49702a5aa8bb6d22a2ec134
Yerin Park <1472moment@gmail.com>
2021-11-11 11:27:24 +0900

    초간단 블로그

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello, Heroku"
+    return render_template("index.html")
+
+
+"""in CMD
+> set FLASK_APP=hello
+> flask run
+"""
+
+
+@app.route("/profile")
+def profile():
+    # DB access
+    like_foods = [
+        "민초",
+        "마라탕",
+        "버블티",
+        "슈바인하겐",
+        "갈릭버터새우",
+    ]
+    return render_template("profile.html", like_foods=like_foods)
+
+
+@app.route("/posts")
+def post_list():
+    return render_template("post_list.html")
